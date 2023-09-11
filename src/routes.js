8c765d19790718b2cabd535f8042ee3ebd388db5
Juan Marcos <jncontatojn@gmail.com>
2023-09-11 17:34:01 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+
+import Footer from "./components/Footer";
+import Home from "./components/pages/Home";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Rola a página para o topo quando a rota muda
+  }, [pathname]);
+
+  return null;
+}
+
+export default function AppRoutes() {
+  return (
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          >
+            <Route
+              index
+              element={<Home />}
+            />
+            <Route
+              path='about'
+              element={<Home />}
+            />
+          </Route>
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
