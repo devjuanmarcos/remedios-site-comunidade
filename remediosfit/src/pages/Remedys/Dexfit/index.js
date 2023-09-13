f006556aa3b6116a106b4acd52931c611e5255ee
Juan Marcos <jncontatojn@gmail.com>
2023-09-12 23:22:19 -0300

    RoundedImageBanner component

--- remediosfit/src/pages/Remedys/Dexfit/index.js
+++ remediosfit/src/pages/Remedys/Dexfit/index.js
@@ -2,12 +2,14 @@ import BannerMain1 from "components/Banners/BannerMain1";
 import React from "react";
 import styles from "./Dexfit.module.css";
 import ThreeIconsCard from "components/Cards/ThreeIcons";
+import RoundedImageBanner from "components/Banners/RoundedImage";
 
 export default function Dexfit() {
   return (
     <div className={styles.bannerMain1}>
       <BannerMain1 />
       <ThreeIconsCard />
+      <RoundedImageBanner />
     </div>
   );
 }
