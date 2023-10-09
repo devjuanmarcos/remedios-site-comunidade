81fd434ac3b8ed925f88954c5fd5d57cac6726fb
Juan Marcos <jncontatojn@gmail.com>
2023-10-09 13:38:15 -0300

    new Price component

--- remediosfit/src/components/Cards/TopCardPlan/index.jsx
+++ remediosfit/src/components/Cards/TopCardPlan/index.jsx
@@ -39,91 +39,206 @@ export default function TopCardPlanCard({ type, id }) {
   return (
     <>
       {filteredBanner.map((banner) => (
-        <div className={styles.container}>
+        // <div className={styles.container}>
+        //   <div
+        //     className={styles.card}
+        //     style={{
+        //       backgroundColor: filteredId.secundaryColor,
+        //     }}
+        //   >
+        //     <img
+        //       className={styles.image}
+        //       src={banner.image1un}
+        //       alt='none'
+        //     />
+        //     <div className={styles.cardContent}>
+        //       <h3 style={{}}>Kit 1 Unidade</h3>
+        //       <div className={styles.price}>
+        //         <h1
+        //           style={{ color: "var(--branco)" }}
+        //         >{`R$ ${banner.price3unCredit}`}</h1>
+        //         <h3>Em 12x sem juros</h3>
+        //       </div>
+        //       <ButtonMain
+        //         color={filteredId.primaryColor}
+        //         children={"Comprar"}
+        //       />
+        //     </div>
+        //   </div>
+        //   <div
+        //     className={styles.cardMain}
+        //     style={{
+        //       backgroundColor: filteredId.secundaryColor,
+        //       boxShadow: `0px 4px 63px 0px ${filteredId.secundaryColor}`,
+        //     }}
+        //   >
+        //     <img
+        //       className={styles.image}
+        //       src={banner.image3un}
+        //       alt='none'
+        //     />
+        //     <div className={styles.cardContent}>
+        //       <h3 style={{}}>Kit 3 Unidades</h3>
+        //       <div className={styles.price}>
+        //         <h1
+        //           style={{ color: "var(--branco)" }}
+        //         >{`R$ ${banner.price3unCredit}`}</h1>
+        //         <h3>Em 12x sem juros</h3>
+        //       </div>
+        //       <ButtonMain
+        //         color={filteredId.primaryColor}
+        //         children={"Comprar"}
+        //       />
+        //     </div>
+        //     <div
+        //       className={styles.promoTape}
+        //       style={{
+        //         backgroundColor: "var(--branco)",
+        //       }}
+        //     >
+        //       <h3 style={{ color: filteredId.secundaryColor, padding: "10px" }}>
+        //         O MAIS PROCURADO
+        //       </h3>
+        //     </div>
+        //   </div>
+        //   <div
+        //     className={styles.card}
+        //     style={{
+        //       backgroundColor: filteredId.secundaryColor,
+        //     }}
+        //   >
+        //     <img
+        //       className={styles.image}
+        //       src={banner.image5un}
+        //       alt='none'
+        //     />
+        //     <div className={styles.cardContent}>
+        //       <h3 style={{}}>Kit 5 Unidades</h3>
+        //       <div className={styles.price}>
+        //         <h1
+        //           style={{ color: "var(--branco)" }}
+        //         >{`R$ ${banner.price3unCredit}`}</h1>
+        //         <h3>Em 12x sem juros</h3>
+        //       </div>
+        //       <ButtonMain
+        //         color={filteredId.primaryColor}
+        //         children={"Comprar"}
+        //       />
+        //     </div>
+        //   </div>
+        // </div>
+        <div className={styles.topCardPlanCard}>
           <div
-            className={styles.card}
-            style={{
-              backgroundColor: filteredId.secundaryColor,
-            }}
-          >
-            <img
-              className={styles.image}
-              src={banner.image1un}
-              alt='none'
-            />
-            <div className={styles.cardContent}>
-              <h3 style={{}}>Kit 1 Unidade</h3>
-              <div className={styles.price}>
-                <h1
-                  style={{ color: "var(--branco)" }}
-                >{`R$ ${banner.price3unCredit}`}</h1>
-                <h3>Em 12x sem juros</h3>
-              </div>
-              <ButtonMain
-                color={filteredId.primaryColor}
-                children={"Comprar"}
-              />
-            </div>
-          </div>
-          <div
-            className={styles.cardMain}
+            className={styles.boxCardMain}
             style={{
               backgroundColor: filteredId.secundaryColor,
               boxShadow: `0px 4px 63px 0px ${filteredId.secundaryColor}`,
             }}
           >
-            <img
-              className={styles.image}
-              src={banner.image3un}
-              alt='none'
-            />
-            <div className={styles.cardContent}>
-              <h3 style={{}}>Kit 3 Unidades</h3>
-              <div className={styles.price}>
-                <h1
-                  style={{ color: "var(--branco)" }}
-                >{`R$ ${banner.price3unCredit}`}</h1>
-                <h3>Em 12x sem juros</h3>
-              </div>
-              <ButtonMain
-                color={filteredId.primaryColor}
-                children={"Comprar"}
+            <div className={styles.cardMainContent}>
+              <h3 style={{ fontSize: "3.35vh" }}>Kit 3 unidades</h3>
+              <img
+                className={styles.imageMainMain}
+                src={banner.image3un}
+                alt='none'
               />
-            </div>
-            <div
-              className={styles.promoTape}
-              style={{
-                backgroundColor: "var(--branco)",
-              }}
-            >
-              <h3 style={{ color: filteredId.secundaryColor, padding: "10px" }}>
-                O MAIS PROCURADO
+              <h3 style={{ fontSize: "2.79vh" }}>
+                De R$ {banner.price3unOld} por apenas:
               </h3>
+              <div className={styles.priceMain}>
+                <h3 style={{ fontSize: "2.79vh" }}>12X</h3>
+                <span className={styles.priceValue}>R$ {banner.price3un}</span>
+              </div>
+              <h3 style={{ color: "var(--branco)" }}>
+                ou R$ {banner.price3un} à vista
+              </h3>
+              <div className={styles.buttonMain}>
+                <h2
+                  className={styles.text}
+                  style={{ color: filteredId.secundaryColor }}
+                >
+                  EU QUERO
+                </h2>
+              </div>
             </div>
           </div>
           <div
-            className={styles.card}
+            className={styles.boxCardMain}
             style={{
               backgroundColor: filteredId.secundaryColor,
+              boxShadow: `0px 4px 63px 0px ${filteredId.secundaryColor}`,
             }}
           >
-            <img
-              className={styles.image}
-              src={banner.image5un}
-              alt='none'
-            />
-            <div className={styles.cardContent}>
-              <h3 style={{}}>Kit 5 Unidades</h3>
-              <div className={styles.price}>
-                <h1
-                  style={{ color: "var(--branco)" }}
-                >{`R$ ${banner.price3unCredit}`}</h1>
-                <h3>Em 12x sem juros</h3>
-              </div>
-              <ButtonMain
-                color={filteredId.primaryColor}
-                children={"Comprar"}
+            <div className={styles.mostPop}>
+              <h1
+                style={
+                  filteredId.secundaryColor
+                    ? { fontSize: "4vh", color: filteredId.secundaryColor }
+                    : { fontSize: "4vh" }
+                }
+              >
+                MAIS POPULAR
+              </h1>
+            </div>
+            <div className={styles.cardMainContent}>
+              <h3 style={{ fontSize: "3.35vh" }}>Kit 3 unidades</h3>
+              <img
+                className={styles.imageMainMain}
+                src={banner.image3un}
+                alt='none'
               />
+              <h3 style={{ fontSize: "2.79vh" }}>
+                De R$ {banner.price3unOld} por apenas:
+              </h3>
+              <div className={styles.priceMain}>
+                <h3 style={{ fontSize: "2.79vh" }}>12X</h3>
+                <span className={styles.priceValue}>R$ {banner.price3un}</span>
+              </div>
+              <h3 style={{ color: "var(--branco)" }}>
+                ou R$ {banner.price3un} à vista
+              </h3>
+              <div className={styles.buttonMain}>
+                <h2
+                  className={styles.text}
+                  style={{ color: filteredId.secundaryColor }}
+                >
+                  EU QUERO
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div
+            className={styles.boxCardMain}
+            style={{
+              backgroundColor: filteredId.secundaryColor,
+              boxShadow: `0px 4px 63px 0px ${filteredId.secundaryColor}`,
+            }}
+          >
+            <div className={styles.cardMainContent}>
+              <h3 style={{ fontSize: "3.35vh" }}>Kit 3 unidades</h3>
+              <img
+                className={styles.imageMainMain}
+                src={banner.image3un}
+                alt='none'
+              />
+              <h3 style={{ fontSize: "2.79vh" }}>
+                De R$ {banner.price3unOld} por apenas:
+              </h3>
+              <div className={styles.priceMain}>
+                <h3 style={{ fontSize: "2.79vh" }}>12X</h3>
+                <span className={styles.priceValue}>R$ {banner.price3un}</span>
+              </div>
+              <h3 style={{ color: "var(--branco)" }}>
+                ou R$ {banner.price3un} à vista
+              </h3>
+              <div className={styles.buttonMain}>
+                <h2
+                  className={styles.text}
+                  style={{ color: filteredId.secundaryColor }}
+                >
+                  EU QUERO
+                </h2>
+              </div>
             </div>
           </div>
         </div>
